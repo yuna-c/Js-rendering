46f8d1568038c1bfb3149338cac4883a3ba7127b
yuna <yuna744@gmail.com>
2024-06-18 15:58:11 +0900

    주석 삭제

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use script';
 
-// 태그 초기화
 let list = ``;
-// 메뉴 리스트
 const menuList = document.querySelector('#menuList');
 
-// 데이터
 const menuItems = [
   { name: '비빔밥', description: '밥 위에 나물, 고기, 고추장 등을 얹고 비벼 먹는 한국 요리' },
   { name: '김치찌개', description: '김치와 돼지고기 등을 넣고 끓인 한국의 찌개 요리' },
@@ -17,29 +14,24 @@ const menuItems = [
   { name: '제육볶음', description: '돼지고기를 고추장, 고춧가루와 양념해 양파와 볶은 한국의 볶음 요리' }
 ];
 
-// 반복문
 const menuElements = menuItems.map((data, idx) => {
-  // 구조분해 할당을 통한 데이터 쪼개기
   const { name, description } =  data;
 
-  if( data.description.includes('고기') ) {
-    return list += `
+  if(data.description.includes('고기')) {
+    list += `
     <li class="color">
       <h3>${data.name}</h3>
       <p>${data.description}</p>
     </li>`;
     } else {
-      return list += `
+      list += `
       <li>
         <h3>${data.name}</h3>
         <p>${data.description}</p>
       </li>`;
     }
-    // console.log(data.description);
-    // console.log(data.description.includes('고기'));
+    
+    return false;
 });
-
-// console.log(list);
-// console.log(menuList);
 
 menuList.innerHTML = list;
